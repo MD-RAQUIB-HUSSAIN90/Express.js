2a023242b71cb6cd6af6952201ccca7c375c8195
MD RAQUIB HUSSAIN <mdraquibhussain71@gmail.com>
2025-09-30 00:53:51 +0530

    second commit

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 const express = require("express");
 const app = express();
 
-
-app.use(function (req, res, next) {
-    next();
-});
-
 app.set("view engine", "ejs")
+app.use(express.static("public"))
 
 app.get('/', function (req, res) {
-    res.render("index", { username: "Arshkhan" });
+    res.render("index");
+})
+app.get('/error', function (req, res, next) {
+    throw Error("this is a forced error");
 })
 
-app.get('/profile', function (req, res) {
-    res.send("hello from profile");
+app.get('/contact', function (req, res) {
+    res.render("contact");
 })
 app.get('/profile/:username/:id', function (req, res) {
     res.send(`this is ${req.params.username} and id no is :${req.params.id}`);
 })
+
+app.use(function errorHandler(err, req, res, next) {
+    if (err.headersSent) {
+        return next(err);
+    }
+    res.status(500);
+    res.render("error", { error: err });
+});
+
 app.listen(3000);
